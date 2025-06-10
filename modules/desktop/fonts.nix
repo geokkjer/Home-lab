aea88565bd44f422278a3102f5fb38fef3f61971
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-06-10 21:51:41 +0200

    moar fonts

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,13 +1,24 @@
-{ config, pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   # Minimal font configuration - users can add more fonts as needed
   fonts.packages = with pkgs; [
     # Essential system fonts
     noto-fonts
     noto-fonts-emoji
     liberation_ttf
-    
-    # One good monospace font for terminals/coding
+    fira-code
+    fira-code-symbols
+    inter-nerdfont
+
+    # Nerd fonts
+    nerd-fonts.iosevka-term
+    nerd-fonts.meslo-lg
     nerd-fonts.jetbrains-mono
+    nerd-fonts.zed-mono
+    nerd-fonts.monaspace
   ];
 
   # Console configuration
